f98824d8d8e3944c288b11eb372bc590b241faa9
Vasiliy Karavaev <vasiliykaravaev@gmail.com>
2022-09-18 13:34:10 +0530

    добаваили

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿
+ Console.Write(" Ввведите массив из 5 значений ");
+
+string [] array1 = new string[5];
+for(int i = 0;i<array1.Length;i++)
+{
+    array1[i]= Console.ReadLine();
+}
+
+string[] array2 = new string[array1.Length];
+
+void Task(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ Task(array1, array2);
+ Console.Write("Mассив из строк, длина которых меньше, либо равна 3 символам = ");
+PrintArray(array2);
